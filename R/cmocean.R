a33aa0efe41edaf543052a356c2715ea94c3cd2d
Ivan K <krylov.r00t@gmail.com>
2020-11-13 19:39:39 +0300

    add 'alpha' and 'direction' arguments for #5

--- R/cmocean.R
+++ R/cmocean.R
@@ -1,9 +1,10 @@
-cmocean <- function(name, version = getOption('cmocean-version', '2.0'), clip = 0, start = clip/2, end = 1 - clip/2) {
+cmocean <- function(
+	name, version = getOption('cmocean-version', '2.0'), clip = 0,
+	start = clip/2, end = 1 - clip/2, direction = 1, alpha = 1
+) {
 	cols <- palettes[[version]][[name]]
-	ncols <- nrow(cols)-1
-	grDevices::colorRampPalette(
-		grDevices::rgb(
-			cols[1 + (ncols*start):(ncols*end),]
-		)
-	)
+	ncols <- nrow(cols) - 1
+	rows <- (ncols*start):(ncols*end)
+	if (direction < 0) rows <- rev(rows)
+	colorRampPalette(rgb(red = cols[1 + rows,], alpha = alpha), alpha = TRUE)
 }
