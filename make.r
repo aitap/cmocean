c5178700c25c211605adf395cf11893c6e58f396
aitap <krylov.r00t@gmail.com>
2020-11-14 11:37:09 +0300

    make.r: exactly reproducible R/sysdata.rda
    save() stores the current R version inside the saved file, so we
    rewrite it to 3.6.3 which was the version we used to change the file
    last time. Also of note should be the compression settings; use xz -lvv
    to see if anything's different.

--- make.r
+++ make.r
@@ -15,6 +15,20 @@ palettes <- local({
 
 save(palettes, file = 'R/sysdata.rda', version = 2, compress = 'xz')
 
+# fix the version number to 3.6.3 to avoid binary changes in git
+f <- xzfile('R/sysdata.rda', 'rb')
+contents <- readBin(f, raw(), 1e7) # should be less than that
+close(f)
+h0 <- c( # see R Internals, 1.8 Serialization Formats
+	charToRaw('RDX2\nX\n'),
+	as.raw(c(0, 0, 0, 2, 0, unclass(getRversion())[[1]]))
+)
+stopifnot(length(contents) < 1e7, contents[seq_along(h0)] == h0)
+contents[length(h0) - 2:0] <- as.raw(c(3, 6, 3))
+f <- xzfile('R/sysdata.rda', 'wb', compression = 9)
+writeBin(contents, f)
+close(f)
+
 unlink(Sys.glob('cmocean_*.tar.gz'))
 system('R CMD build .')
 
