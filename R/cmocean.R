9921e998b4046b8b2d4a9c32fe7a73b781d4e087
Ilja Kocken <i.j.kocken@uu.nl>
2020-11-15 16:16:17 +0100

    simplify 'name' parameter checks

--- R/cmocean.R
+++ R/cmocean.R
@@ -2,33 +2,7 @@ cmocean <- function(
 	name, version = getOption('cmocean-version', '2.0'), clip = 0,
 	start = clip/2, end = 1 - clip/2, direction = 1, alpha = 1
 ) {
-	 if (!name %in% c(
-         	"thermal",
-         	"haline",
-          	"solar",
-          	"ice",
-          	"gray",
-          	"oxy",
-          	"deep",
-          	"dense",
-          	"algae",
-          	"matter",
-          	"turbid",
-          	"speed",
-          	"amp",
-          	"tempo",
-          	"rain",
-          	"phase",
-          	"topo",
-          	"balance",
-          	"delta",
-          	"curl",
-          	"diff",
-          	"tarn")) {
-            warning(paste0("Name '", name, "' does not exist. Defaulting to 'thermal'."))
-            name <- "thermal"
-        }
-
+        name <- match.arg(name, names(palettes[[version]]))
 	cols <- palettes[[version]][[name]]
 	ncols <- nrow(cols) - 1
 	rows <- (ncols*start):(ncols*end)
