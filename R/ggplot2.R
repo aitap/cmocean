97280a150557bf410d698b25ecb82903249e0ab5
Ivan K <krylov.r00t@gmail.com>
2020-11-16 13:15:47 +0300

    add the version argument to ggplot2 wrappers (#5)

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -1,21 +1,21 @@
 scale_fill_cmocean <- function(
-	..., alpha = 1, start = 0, end = 1, direction = 1,
-	discrete = FALSE, name = 'thermal'
+	..., alpha = 1, start = 0, end = 1, direction = 1, discrete = FALSE,
+	name = 'thermal', version = getOption('cmocean-version', '2.0')
 ) {
 	loadNamespace('ggplot2')
 	if (discrete) {
 		ggplot2::discrete_scale(
 			'fill', 'cmocean', cmocean(
-				name = name, start = start, end = end,
-				direction = direction, alpha = alpha
+				name = name, version = version, start = start,
+				end = end, direction = direction, alpha = alpha
 			),
 			...
 		)
 	} else {
 		ggplot2::scale_fill_gradientn(
 			colours = cmocean(
-				name = name, start = start, end = end,
-				direction = direction, alpha = alpha
+				name = name, version = version, start = start,
+				end = end, direction = direction, alpha = alpha
 			)(256),
 			...
 		)
@@ -23,23 +23,23 @@ scale_fill_cmocean <- function(
 }
 
 scale_colour_cmocean <- scale_color_cmocean <- function(
-	..., alpha = 1, start = 0, end = 1, direction = 1,
-	discrete = FALSE, name = 'thermal'
+	..., alpha = 1, start = 0, end = 1, direction = 1, discrete = FALSE,
+	name = 'thermal', version = getOption('cmocean-version', '2.0')
 ) {
 	loadNamespace('ggplot2')
 	if (discrete) {
 		ggplot2::discrete_scale(
 			'colour', 'cmocean', cmocean(
-				name = name, start = start, end = end,
-				direction = direction, alpha = alpha
+				name = name, version = version, start = start,
+				end = end, direction = direction, alpha = alpha
 			),
 			...
 		)
 	} else {
 		ggplot2::scale_color_gradientn(
 			colours = cmocean(
-				name = name, start = start, end = end,
-				direction = direction, alpha = alpha
+				name = name, version = version, start = start,
+				end = end, direction = direction, alpha = alpha
 			)(256),
 			...
 		)
